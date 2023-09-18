b5db0e56956cbd03711fe77b4b05cc8f9b68d44b
Olivia Guyot <olivia.guyot@camptocamp.com>
2023-09-18 13:24:34 +0200

    geocat: add location filter in search & location input in home page
    Also change org strategy to "Groups"

--- libs/feature/router/src/lib/default/services/router-search.service.spec.ts
+++ libs/feature/router/src/lib/default/services/router-search.service.spec.ts
@@ -1,4 +1,8 @@
-import { FieldsService, SearchFacade } from '@geonetwork-ui/feature/search'
+import {
+  FieldsService,
+  LocationBbox,
+  SearchFacade,
+} from '@geonetwork-ui/feature/search'
 import {
   SortByEnum,
   SortByField,
@@ -20,6 +24,7 @@ class SearchFacadeMock {
 class RouterFacadeMock {
   setSearch = jest.fn()
   updateSearch = jest.fn()
+  go = jest.fn()
 }
 
 class FieldsServiceMock {
@@ -46,6 +51,10 @@ class FieldsServiceMock {
       }, {})
     )
   )
+}
+
+class RouterServiceMock {
+  getSearchRoute = jest.fn().mockReturnValue('/test/path')
 }
 
 describe('RouterSearchService', () => {
@@ -164,6 +173,42 @@ describe('RouterSearchService', () => {
           publisher: ['mel'],
           _sort: '-_score',
         })
+      })
+    })
+  })
+
+  describe('#setLocationFilter', () => {
+    beforeEach(() => {
+      const location: LocationBbox = {
+        label: 'New location',
+        bbox: [4, 5, 6, 7],
+      }
+      service.setLocationFilter(location)
+    })
+    it('dispatch setLocationFilter with merged mapped params', () => {
+      expect(routerFacade.go).toHaveBeenCalledWith({
+        path: '/test/path',
+        query: {
+          location: 'New location',
+          bbox: '4,5,6,7',
+        },
+        queryParamsHandling: 'merge',
+      })
+    })
+  })
+
+  describe('#clearLocationFilter', () => {
+    beforeEach(() => {
+      service.clearLocationFilter()
+    })
+    it('dispatch clearLocationFilter with merged mapped params', () => {
+      expect(routerFacade.go).toHaveBeenCalledWith({
+        path: '/test/path',
+        query: {
+          location: undefined,
+          bbox: undefined,
+        },
+        queryParamsHandling: 'merge',
       })
     })
   })
