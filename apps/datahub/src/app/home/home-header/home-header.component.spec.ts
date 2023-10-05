4700da01fcd1650fbf9b075861c667be220459e9
Florian Necas <florian.necas@camptocamp.com>
2023-10-05 09:50:06 +0200

    geocat: add geocat-header with langage switcher

--- apps/datahub/src/app/home/home-header/home-header.component.spec.ts
+++ apps/datahub/src/app/home/home-header/home-header.component.spec.ts
@@ -280,7 +280,8 @@ describe('HomeHeaderComponent', () => {
         })
       })
 
-      describe('language switcher', () => {
+      // skipped for geocat
+      describe.skip('language switcher', () => {
         describe('given predefined languages', () => {
           it('should display language switcher', () => {
             const languageSwitcher = fixture.debugElement.queryAll(
